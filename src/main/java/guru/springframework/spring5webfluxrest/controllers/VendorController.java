750fedb907b4fc2d42f046dc756e9d58e86e82a9
Tangi Vass <tangi.vass.ep@biznet.io>
2019-01-15 11:14:11 +0100

    Create Get Endpoints for Vendors

---
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -0,0 +1,32 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/vendors")
+public class VendorController {
+
+    private final VendorRepository vendorRepository;
+
+    public VendorController(VendorRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
+
+    @GetMapping
+    Flux<Vendor> list(){
+        return vendorRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Mono<Vendor> getById(@PathVariable String id){
+        return vendorRepository.findById(id);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,56 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.BDDMockito.given;
+
+public class VendorControllerTest {
+
+    WebTestClient webTestClient;
+
+    @Mock
+    VendorRepository vendorRepository;
+
+    @InjectMocks
+    VendorController vendorController;
+
+    private final String BASE_URL = "/api/v1/vendors";
+    private final Vendor VENDOR_1 = Vendor.builder().firstName("Fred").firstName("Flintstone").build();
+    private final Vendor VENDOR_2 = Vendor.builder().firstName("Barney").firstName("Rubble").build();
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+    }
+
+    @Test
+    public void list() {
+        given(vendorRepository.findAll())
+                .willReturn(Flux.just(VENDOR_1,VENDOR_2));
+
+        webTestClient.get()
+                .uri(BASE_URL)
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        given(vendorRepository.findById("someid"))
+                .willReturn(Mono.just(VENDOR_1));
+
+        webTestClient.get()
+                .uri(BASE_URL + "/someid")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+}
